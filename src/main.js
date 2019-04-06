b2ff0246ac42f8c5ff9a22f860ad5348cadda0fd
Zevi Liao <jointjm@gmail.com>
2019-04-06 11:58:09 +0800

    add mock up

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/app.scss'
+import './mock' // simulation data
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
---
+++ src/mock/login.js
@@ -0,0 +1,20 @@
+import { param2Obj } from '@/utils'
+import json from './json/data.json'
+
+const userMap = json
+
+export default {
+  loginByUsername: config => {
+    const { username } = JSON.parse(config.body)
+    return userMap[username]
+  },
+  getUserInfo: config => {
+    const { token } = param2Obj(config.url)
+    if (userMap[token]) {
+      return userMap[token]
+    } else {
+      return false
+    }
+  },
+  logout: () => 'success'
+}
