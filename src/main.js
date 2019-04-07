ad92c0a37e0fb4a0ef5047968a15d706173167e8
Zevi Liao <jointjm@gmail.com>
2019-04-07 15:31:12 +0800

    remove bV

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap'
+// import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -7,7 +8,7 @@ import './styles/app.scss'
 import './mock' // simulation data
 import 'font-awesome/css/font-awesome.css'
 
-Vue.use(BootstrapVue)
+// Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
